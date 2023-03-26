78957b41087b056fa5c18c952c2eaff68f00770a
UjjwalPasahan <96993068+UjjwalPasahan@users.noreply.github.com>
2023-03-27 01:30:16 +0530

    changed button hover

--- client/src/Components/LoginNav.js
+++ client/src/Components/LoginNav.js
@@ -3,17 +3,13 @@ import { Nav, Navbar, NavbarText, NavItem } from "reactstrap";
 
 export default function LoginNav({ msg }) {
 	return (
-		<Navbar style={{ backgroundColor: "#242526" }} expand="md">
+		<Navbar style={{ backgroundColor: "#fff" }} expand="md">
 			<Nav className="mr-auto" navbar>
 				<NavItem
-					style={{
-						color: "white",
-						padding: "20px",
-						fontSize: "20px",
-					}}
+					style={{padding: "20px",fontSize: "20px"}}
 				>
 					<Link
-						style={{ textDecoration: "none", color: "white" }}
+						style={{ textDecoration: "none" }}
 						to="/"
 					>
 						Home
@@ -24,8 +20,8 @@ export default function LoginNav({ msg }) {
 			{msg ? (
 				<></>
 			) : (
-				<NavbarText>
-					<Link to="/signUp" style={{ color: "white" }}>
+				<NavbarText >
+					<Link to="/signUp" >
 						New User? SignUp!!
 					</Link>
 				</NavbarText>
