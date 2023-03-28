34b7718edf4b2a081d69a02ff4fd802018eea94c
Catalystde <110329965+Catalystde@users.noreply.github.com>
2023-03-28 18:15:59 +0530

    ggsipu at maps

--- client/src/Components/contact.js
+++ client/src/Components/contact.js
@@ -14,14 +14,14 @@ function Contact() {
                 </div>
 
                 <div>
-                    <iframe style={{border:"0", width: "100%", height: "350px" }} src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" allowfullscreen></iframe>
+                    <iframe style={{border:"0", width: "100%", height: "350px" }} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3503.1600870883726!2d77.0171102150817!3d28.594973882432694!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d1aafdb891567%3A0x10d270731c930a87!2sGuru%20Gobind%20Singh%20Indraprastha%20University!5e0!3m2!1sen!2sin!4v1680007417655!5m2!1sen!2sin" width="600" height="450"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" frameborder="0"  ></iframe>
                 </div>
 
-                <div className="container">
+                <div className="container flex" style={{flexDirection:"row"}} >
                     <div className="row mt-5">
 
-                        <div className="col-lg-4" style={{textAlign:"start",float:"left",flex:"start",margin:"0",padding:"0"}}>
-                            <div className="info">
+                        <div className="col-lg-4" >
+                            <div className="info" style={{textAlign:"start",float:"left",flex:"start",marginRight:"90px",padding:"0"}}>
                                 <div className="address">
                                     <i className="bi bi-geo-alt"></i>
                                     <h4>Location:</h4>
@@ -61,11 +61,11 @@ function Contact() {
                                 <div className="form-group mt-3">
                                     <textarea className="form-control" name="message" rows="5" placeholder="Message" required></textarea>
                                 </div>
-                                <div className="my-3">
+                                {/* <div className="my-3">
                                     <div className="loading">Loading</div>
                                     <div className="error-message"></div>
                                     <div className="sent-message">Your message has been sent. Thank you!</div>
-                                </div>
+                                </div> */}
                                 <div className="text-center"><button type="submit">Send Message</button></div>
                             </form>
 
